fa7e2b2d4c96c3f965a8bd671ef1f30276c95b86
bekzod1337 <rahimjonovazarina@gmail.com>
2025-08-01 16:33:46 +0500

    vercel problemm

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import CarouselSection from "./components/CarouselSection";
 import FeatureImageSection from "./components/FeatureImageSection";
 import FeaturesSwiperSection from "./components/Features";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import PersonalizeSection from "./components/PersonalizeSection";
 
